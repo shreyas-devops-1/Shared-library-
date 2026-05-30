ca09830ead88b8f685b2c1447d01000736fdca66
Shreyas Awasthi <shreyasawasthi398@gmail.com>
2026-05-30 23:38:47 +0530

    Update terraformValidate.groovy

--- vars/terraformValidate.groovy
+++ vars/terraformValidate.groovy
@@ -1,6 +1,12 @@
-def call() {
-    sh '''
-        terraform init -backend=false
-        terraform validate
-    '''
+def call(Map config = [:]) {
+
+    String workingDir = config.workingDir ?: '.'
+
+    dir(workingDir) {
+
+        sh '''
+            terraform init -backend=false
+            terraform validate
+        '''
+    }
 }
